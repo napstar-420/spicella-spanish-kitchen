02cb0b3bc3e81d55426bc659212971583ea0351d
zohaib khan <zohaibkhan1162002@gmail>
2022-07-08 05:32:31 -0700

    Made Header section

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,7 +1,51 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import { BsArrowRightShort } from "react-icons/bs";
+import { TbHelp } from "react-icons/tb";
+import { IoIosArrowDown } from "react-icons/io";
+import { FaFacebookSquare, FaTwitterSquare, FaInstagramSquare, FaYoutubeSquare, FaMapMarkerAlt } from "react-icons/fa";
 
 const Home = () => {
-  return <header>Home</header>;
+  return (
+    <div className="home">
+        <header className="header-container">
+            <div className="top-nav">
+                <ul>
+                    <li>&reg; Since 1956</li>
+                    <li>Help <TbHelp style={{margin: '0 0.5rem'}}/></li>
+                </ul>
+            </div>
+            <div className="middle-container">
+                <div className='image-back'/>
+                <div className="welcome-container">
+                    <h1>Spicella Spanish Kitchen</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi ad aut iusto at molestias repudiandae pariatur rem similique magnam asperiores amet fuga fugiat enim debitis error earum nisi quos ullam, dolores sed eius voluptate!</p>
+                    <div className="buttons">
+                        <button className="header-btn menu-btn">Menu</button>
+                        <button className="header-btn order-btn">Order Online <BsArrowRightShort className="order-icon"/></button>
+                    </div>
+                    <nav>
+                        <a href="#">Home <IoIosArrowDown className="nav-link-icon"/></a>
+                        <a href="#">About us <IoIosArrowDown className="nav-link-icon"/></a>
+                        <a href="#">Menu <IoIosArrowDown className="nav-link-icon"/></a>
+                        <a href="#">Services <IoIosArrowDown className="nav-link-icon"/></a>
+                        <a href="#">Contact <IoIosArrowDown className="nav-link-icon"/></a>
+                        <a href="#">Rate us <IoIosArrowDown className="nav-link-icon"/></a>
+                    </nav>
+                    <div className="bottom-container">
+                        <button><FaMapMarkerAlt/> Locate us</button>
+                        <div className="icons">
+                            <FaFacebookSquare className="social-icons"/>
+                            <FaInstagramSquare className="social-icons"/>
+                            <FaTwitterSquare className="social-icons"/>
+                            <FaYoutubeSquare className="social-icons"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+  )
 };
 
 export default Home;
