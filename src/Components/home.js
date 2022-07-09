be54db2b8c16181c920770d1c0cf5db26bcd0f4f
zohaib khan <zohaibkhan1162002@gmail>
2022-07-09 04:07:36 -0700

    Creat menu section

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,11 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, {useState} from "react";
+import { menuItems } from "./menu";
 import { BsArrowRightShort } from "react-icons/bs";
 import { TbHelp } from "react-icons/tb";
 import { IoIosArrowDown } from "react-icons/io";
-import { FaFacebookSquare, FaTwitterSquare, FaInstagramSquare, FaYoutubeSquare, FaMapMarkerAlt, FaRegHeart } from "react-icons/fa";
+import { FaFacebookSquare, FaTwitterSquare, FaInstagramSquare, FaYoutubeSquare, FaMapMarkerAlt, FaRegHeart, FaHeart, } from "react-icons/fa";
 
 const Home = () => {
+
+    const [favorite, setFavorite] = useState(false)
 
     const sectionStyle = {
         width: '90%',
@@ -81,9 +84,23 @@ const Home = () => {
                 <p style={paraStyle}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ipsam nemo fugiat consequuntur. Reprehenderit nam qui, vel dolor repellendus provident impedit maiores numquam ipsa facilis ipsam obcaecati eligendi velit illo?</p>
             </section>
             <div className="underline"></div>
+            <h1 style={{textAlign: 'center', fontFamily: 'Albert Sans, sans-serif'}}>Menu</h1>
             <section className="menu-container">
-                <h1>Menu</h1>
-                
+                {
+                    menuItems.map((item, index)=>{
+                        const {name, url} = item;
+                        return(
+                            <div className="item-container" key={index} >
+                                <img src={url} alt={name} />
+                                <div className="content" style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0.3rem 0 0.5rem 0'}}>
+                                    <h2 style={{fontFamily: 'Roboto, sans-serif'}}>{name}</h2>
+                                    {favorite ? <FaHeart type="button"  style={{fontSize: '1.5rem', cursor: 'pointer'}} onClick={()=>{setFavorite(!favorite)}}/>: <FaRegHeart type="button" style={{fontSize: '1.5rem', cursor: 'pointer'}} onClick={()=>{setFavorite(!favorite)}}/>}
+                                </div>
+                                <p style={{fontFamily: 'Poppins, sans-serif', fontWeight: '600'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto porro repellat quaerat maiores hic. Magni autem tenetur dolor libero porro!</p>
+                            </div>
+                        )
+                    })
+                }
             </section>
         </main>
     </div>
