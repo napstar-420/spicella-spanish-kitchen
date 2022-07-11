ddc34f882b35a1525697528bb0615be7dc00915a
zohaib khan <zohaibkhan1162002@gmail>
2022-07-11 12:24:31 -0700

    create footer component

---
+++ src/Components/Footer_container.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const FooterContainer = () => {
+  return (
+    <footer>
+      <h1><span>Spicella Spanish Kitchen</span> | &reg; 2022</h1>
+      <div className="footer-links">
+        <a href="#">Terms and Conditions</a>
+        <a href="#">Privacy Policy</a>
+        <a href="#">Work with us</a>
+        <a href="#">Our History</a>
+        <a href="#">Email</a>
+        <a href="#">News Letter</a>
+      </div>
+    </footer>
+  );
+};
+
+export default FooterContainer;
