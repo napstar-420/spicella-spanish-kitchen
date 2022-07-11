d8dbcdcbee939ea1ce63b321bae32820dbf1464a
zohaib khan <zohaibkhan1162002@gmail>
2022-07-11 04:38:29 -0700

    Create Separate Component (Services)

---
+++ src/Components/Services_Container.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+const ServicesContainer = () => {
+  return (
+    <>
+      <h1
+        style={{ textAlign: "center", fontFamily: "Albert Sans, sans-serif" }}
+        id="services"
+      >
+        Services
+      </h1>
+      <section className="services-container">
+        <div className="content">
+          <h2>Booking for an Occasion</h2>
+          <h3>
+            Providing World Class Enivroment whether its a meeting or a Birthday
+            Party
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+            nostrum aliquid aspernatur. Suscipit, quos fugiat. Minima, esse
+            repellat nisi iste ad vel et iure at doloremque porro soluta
+            laborum, libero vero est culpa, saepe blanditiis? Reiciendis vero
+            explicabo odit eaque enim? Dolores aliquid quasi odio consequuntur
+            quaerat, natus ullam ut?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse,
+            illum? Sint magni itaque quo quas sequi dicta ut, cupiditate tempora
+            obcaecati animi nisi laboriosam vitae eveniet porro fuga commodi
+            veniam eligendi non voluptate amet eos pariatur illo! Magnam placeat
+            natus magni dolore molestiae repudiandae tenetur quis illo soluta
+            itaque ab quaerat iure, cupiditate, ratione dicta nisi doloremque
+            ducimus, quam dolor? Magni cupiditate impedit quae optio a iure
+            exercitationem fuga quidem.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
+            impedit delectus sed quibusdam, ipsum nesciunt accusamus maiores
+            ullam repudiandae minima mollitia assumenda adipisci perferendis
+            itaque rerum voluptas ad voluptatem cumque quisquam harum natus
+            magni doloribus voluptatum? Dolorum, perspiciatis! Saepe, dolores.
+          </p>
+        </div>
+      </section>
+      <div className="sub-service-container">
+        <div className="chef sub-service-item">
+          <h2>Our Chef's</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+            veritatis quisquam fuga velit molestias aut odit quos veniam dolores
+            minus.
+          </p>
+        </div>
+        <div className="waiter sub-service-item">
+          <h2>Our Waiter's</h2>
+          <p>
+            LPorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptatem. Facilis rem porro incidunt dolores!
+          </p>
+        </div>
+        <div className="staff sub-service-item">
+          <h2>Our Staff</h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus,
+            numquam hic animi soluta veniam sit modi officiis, recusandae
+            mollitia vel aspernatur nemo dolor obcaecati architecto.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ServicesContainer;
--- src/Components/home.js
+++ src/Components/home.js
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "./HeaderContainer.js";
 import AboutUs from "./About_us.js";
 import Menu from "./Menu_Container.js";
+import Services from "./Services_Container.js";
 
 const Home = () => {
   const sectionStyle = {
@@ -65,71 +66,7 @@ const Home = () => {
 
         <div className="underline" />
 
-        <h1
-          style={{ textAlign: "center", fontFamily: "Albert Sans, sans-serif" }}
-          id="services"
-        >
-          Services
-        </h1>
-        <section className="services-container">
-          <div className="content">
-            <h2>Booking for an Occasion</h2>
-            <h3>
-              Providing World Class Enivroment whether its a meeting or a
-              Birthday Party
-            </h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-              nostrum aliquid aspernatur. Suscipit, quos fugiat. Minima, esse
-              repellat nisi iste ad vel et iure at doloremque porro soluta
-              laborum, libero vero est culpa, saepe blanditiis? Reiciendis vero
-              explicabo odit eaque enim? Dolores aliquid quasi odio consequuntur
-              quaerat, natus ullam ut?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse,
-              illum? Sint magni itaque quo quas sequi dicta ut, cupiditate
-              tempora obcaecati animi nisi laboriosam vitae eveniet porro fuga
-              commodi veniam eligendi non voluptate amet eos pariatur illo!
-              Magnam placeat natus magni dolore molestiae repudiandae tenetur
-              quis illo soluta itaque ab quaerat iure, cupiditate, ratione dicta
-              nisi doloremque ducimus, quam dolor? Magni cupiditate impedit quae
-              optio a iure exercitationem fuga quidem.
-            </p>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
-              impedit delectus sed quibusdam, ipsum nesciunt accusamus maiores
-              ullam repudiandae minima mollitia assumenda adipisci perferendis
-              itaque rerum voluptas ad voluptatem cumque quisquam harum natus
-              magni doloribus voluptatum? Dolorum, perspiciatis! Saepe, dolores.
-            </p>
-          </div>
-        </section>
-        <div className="sub-service-container">
-          <div className="chef sub-service-item">
-            <h2>Our Chef's</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
-              veritatis quisquam fuga velit molestias aut odit quos veniam
-              dolores minus.
-            </p>
-          </div>
-          <div className="waiter sub-service-item">
-            <h2>Our Waiter's</h2>
-            <p>
-              LPorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Quisquam, voluptatem. Facilis rem porro incidunt dolores!
-            </p>
-          </div>
-          <div className="staff sub-service-item">
-            <h2>Our Staff</h2>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus,
-              numquam hic animi soluta veniam sit modi officiis, recusandae
-              mollitia vel aspernatur nemo dolor obcaecati architecto.
-            </p>
-          </div>
-        </div>
+        <Services />
         <div className="underline"></div>
       </main>
     </div>
