663f4dc121f1ce60677748a85228daef24b5563f
zohaib khan <zohaibkhan1162002@gmail>
2022-07-13 11:19:56 -0700

    make separate comp for intro

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import Header from "./HeaderContainer.js";
+import IntroCont from "./main_top_container";
 import AboutUs from "./About_us.js";
 import Menu from "./Menu_Container.js";
 import Services from "./Services_Container.js";
@@ -12,46 +13,13 @@ const Home = () => {
     <div className="home">
       <Header />
       <main>
-        <section id="home">
-          <article>
-            <h1
-              style={{
-                fontSize: "2rem",
-                marginBottom: "1rem",
-                fontFamily: "Albert Sans, sans-serif",
-                color: "#e67f09",
-              }}
-            >
-              Spicella Spanish Kitchen
-            </h1>
-            <p
-              style={{
-                fontSize: "1.2rem",
-                fontFamily: "Albert Sans, sans-serif",
-              }}
-            >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-              obcaecati deleniti illum odio molestiae ad vero, similique, neque
-              quidem at nihil excepturi cum quae alias quis optio ullam
-              architecto fuga necessitatibus accusantium error, magni in. Totam
-              nobis, maiores magnam quod animi quisquam labore quasi dolores
-              necessitatibus iure perferendis, amet molestiae, excepturi sint
-              saepe architecto culpa?
-            </p>
-          </article>
-          <img
-            src="https://images.unsplash.com/photo-1554118811-1e0d58224f24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1447&q=80"
-            width="40%"
-            alt="store-img"
-          />
-        </section>
+        <IntroCont />
         <AboutUs />
         <Menu />
-        <div className="underline" />
         <Services />
         <ContactUs />
-        <Footer />
       </main>
+      <Footer />
     </div>
   );
 };
